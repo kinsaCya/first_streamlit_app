1caadad312a996b791cc49431e6c372b9bf996fe
kinsaCya <119957360+kinsaCya@users.noreply.github.com>
2023-05-30 10:59:56 +0800

    Update streamlit_app.py
    Adding new header to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,11 @@ import streamlit
 
 streamlit.title("My Parents New Healty Diner")
 
-streamlit.header("reakfast Menu")
+streamlit.header("Breakfast Menu")
 
 streamlit.text("🥣 Omega 3 & blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
