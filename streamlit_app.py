cda525d648d515e94384b324c476e5a08f479656
kinsaCya <119957360+kinsaCya@users.noreply.github.com>
2023-05-30 10:53:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & blueberry Oatmeal")
 
 streamlit.text("Hard-Boiled Free-Range Egg")
-streamlit.text("Kale,")
+streamlit.text("Kale, Spinach &")
 
