9cf5013cf21ccfaa26ac577a2f99f48f5181a26d
kinsaCya <119957360+kinsaCya@users.noreply.github.com>
2023-05-30 10:49:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,4 @@ streamlit.title("My Parents New Healty Diner")
 
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & blueberry Oatmeal")
-streamlit.test("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+
