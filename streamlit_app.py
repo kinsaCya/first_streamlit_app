adc1ff113437e07c2a419c032d20a20e0db8ba27
kinsaCya <119957360+kinsaCya@users.noreply.github.com>
2023-05-30 10:54:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title("My Parents New Healty Diner")
 
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & blueberry Oatmeal")
-
-streamlit.text("Hard-Boiled Free-Range Egg")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
+
 
