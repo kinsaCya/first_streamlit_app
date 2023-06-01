4f03660b5e190efd89a4600953908b96d4199109
kinsaCya <119957360+kinsaCya@users.noreply.github.com>
2023-06-01 16:39:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,8 +55,14 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.header("Fruityvice Fruit Advice!")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+#import request
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
+#streamlit.text(fruityvice_response.json())
+
 
 #This will not work correctly, but just go with it for now
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
