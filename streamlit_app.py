e0570df20300cab37d888006f8d1c6112104fbda
kinsaCya <119957360+kinsaCya@users.noreply.github.com>
2023-06-01 16:29:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,13 +28,13 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_selected = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('Thanks for adding ', fruit_selected)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('Thanks for adding ', fruit_choice)
 
 
 
 #import request
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_selected)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json()) #just writes the data to the screen -- should be deleted
 
 # write your own comment -what does the next line do? 
